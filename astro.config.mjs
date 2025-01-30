94b386aa051f0e1783893db334bdbd44babe5334
Cass Fridkin <cass@swag.lgbt>
2025-01-30 13:03:20 -0700

    start styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from "astro/config";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [icon()],
+  env: {
+    schema: {
+      PHONE_NUMBER: envField.number({
+        context: "server",
+        access: "public",
+        optional: true,
+      }),
+      EMAIL_ADDRESS: envField.string({
+        context: "server",
+        access: "public",
+        optional: true,
+      }),
+    },
+  },
+});
